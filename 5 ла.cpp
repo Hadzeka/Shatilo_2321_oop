252fd106860ad39ca4da7507b1243c81580645a5
Hadzeka <166939655+Hadzeka@users.noreply.github.com>
2024-12-14 18:58:12 +0300

    первая часть лабы вроде

---
+++ 5 ла.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class MyString {
+private:
+    string value;
+public:
+    void input() {
+        cout << "Введите строку: ";
+        getline(cin, value);
+    }
+
+    friend ostream& operator<<(ostream& os, const MyString& str);
+};
+
+ostream& operator<<(ostream& os, const MyString& str) {
+    for (char ch : str.value) {
+        if (!isupper(ch) && !isdigit(ch)) {
+            os << ch;
+        }
+    }
+    return os;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "rus");
+
+    MyString myStr;
+    myStr.input();
+    cout << "Строка без заглавных букв и цифр: " << myStr << endl;
+
+    return 0;
+}
